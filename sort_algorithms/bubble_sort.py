ab9e17959f1afe8dba55fa0dfc176615c9bf14c4
mojtaba karimi <mojtaba.python@gmail.com>
2026-06-13 02:54:23 -0700

    Update bubble_sort.py

--- sort_algorithms/bubble_sort.py
+++ sort_algorithms/bubble_sort.py
@@ -1,1 +1,19 @@
 
+from random import randint as rnd
+import matplotlib.pyplot as plt
+
+length=20
+_list=[rnd(0,length) for i in range(length)]
+for i in range(length-1):
+    flag = True
+    for j in range(length-i-1):
+        if _list[j] > _list[j+1]:
+            flag=False
+            _list[j], _list[j+1] = _list[j+1], _list[j]
+        plt.bar(range(length),_list)
+        plt.pause(0.005)
+        plt.clf()
+    if flag:
+        break
+plt.bar(range(length),_list)
+plt.show()
