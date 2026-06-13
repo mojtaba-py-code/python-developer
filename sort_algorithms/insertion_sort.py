71649e16e3a4ef11bdf4eb917da122ef59ba1936
mojtaba karimi <mojtaba.python@gmail.com>
2026-06-13 02:55:02 -0700

    Update insertion_sort.py

--- sort_algorithms/insertion_sort.py
+++ sort_algorithms/insertion_sort.py
@@ -1,1 +1,21 @@
 
+from random import shuffle
+import matplotlib.pyplot as plt
+
+length=100
+_list=[i for i in range(length)]
+shuffle(_list)
+print(_list)
+for i in range(1,length):
+    for j in range(i):
+        if _list[j]>_list[i]:
+            temp=_list[i]
+            del _list[i]
+            _list.insert(j,temp)
+            break
+    plt.bar(range(length),_list)
+    plt.pause(0.005)
+    plt.clf()
+plt.bar(range(length),_list)
+plt.show()
+
