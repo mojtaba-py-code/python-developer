4fff3d742659d2815fb910e4c3a9bed7164ce803
mojtaba karimi <mojtaba.python@gmail.com>
2026-06-13 02:56:07 -0700

    Update Quick_sort.py

--- sort_algorithms/Quick_sort.py
+++ sort_algorithms/Quick_sort.py
@@ -1,1 +1,25 @@
 
+from random import shuffle
+
+length=10
+_list=[i for i in range(length)]
+shuffle(_list)
+print(_list)
+
+def quick_sort(_list,low,high):
+    if low<high:
+        pivot=low
+        i=low
+        j=high
+        while i<j:
+            while _list[i] <= _list[pivot] and i <high:
+                i+=1
+            while _list[j] > _list[pivot]:
+                j-=1
+            if i<j:
+                _list[i], _list[j] = _list[j], _list[i]
+        _list[j], _list[pivot] = _list[pivot], _list[j]
+        quick_sort(_list, low, j-1)
+        quick_sort(_list, j+1, high)
+    return _list
+print(quick_sort(_list,0,len(_list)-1))
