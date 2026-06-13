79dbd453d31f274bbc7a34bb40877d8287ac4791
mojtaba karimi <mojtaba.python@gmail.com>
2026-06-13 02:59:07 -0700

    Update binary_search.py

--- search_algorithms/binary_search.py
+++ search_algorithms/binary_search.py
@@ -1,1 +1,18 @@
 
+_list=[2,4,5,6,7,8,11,16]
+def binary_serch(_list,k):
+    if len(_list)<2:
+        if _list[0]==k:
+            return "found"
+        else:
+            return "not found"
+    else:
+        mid=len(_list)//2
+        if _list[mid]==k:
+            return "found"
+        elif _list[mid]>k:
+            return binary_serch(_list[:mid],k)
+        else:
+            return binary_serch(_list[mid:],k)
+#print(binary_serch(_list,4))
+
