f3f3cfc29f5ffbf5cdcfd2d094019f8702d793cd
mojtaba karimi <mojtaba.python@gmail.com>
2026-06-13 02:56:48 -0700

    Update selection_sort.py

--- sort_algorithms/selection_sort.py
+++ sort_algorithms/selection_sort.py
@@ -1,1 +1,17 @@
 
+from random import randint as rnd
+import matplotlib.pyplot as plt
+
+length=100
+_list=[rnd(0,length) for i in range(length)]
+for i in range(length-1):
+    _min=i
+    for j in range(i+1,length):
+        if _list[_min] > _list[j]:
+            _min=j
+    _list[i], _list[_min] = _list[_min], _list[i]
+    plt.bar(range(length),_list)
+    plt.pause(0.005)
+    plt.clf()
+plt.bar(range(length),_list)
+plt.show()
